fd5b7952744c75153a1bd9cd87f5fb1517378d76
Ed Smith <smith981@gmail.com>
2013-01-30 16:15:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,34 +2,70 @@
 
 require_once(__DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
-//error_reporting(E_ALL);
-//ini_set('display_erors', 1);
-
-use \Symfony\Component\HttpFoundation\Request;
-use \Symfony\Component\HttpFoundation\Response;
-use \Doctrine\Common\Cache\ApcCache;
-use \Doctrine\Common\Cache\ArrayCache;
+error_reporting(E_ALL);
+ini_set('display_erors', 1);
+use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Common\Cache\ApcCache;
+use Doctrine\Common\Cache\ArrayCache;
+use Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
 
 $app = new Silex\Application;
 
-//$app['debug'] = true;
+$app['debug'] = true;
 
 /**
  * Register Doctrine DBAL if needed
  */
-/*
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 
     // Doctrine DBAL settings goes here
     'db.options' => array(
   		'driver'   => 'pdo_mysql',
-  		'user'     => 'username',
-  		'password' => 'pass',
-  		'dbname'   => 'dbname',
+  		'user'     => 'testuser',
+  		'password' => 'secret',
+  		'dbname'   => 'test',
   		'host'	 => 'localhost'
   	)
 ));
-*/
+
+/**
+ * Register the ORM if needed
+ */
+$app->register(new DoctrineOrmServiceProvider, array(
+    "orm.proxies_dir" => "/proxies",
+    "orm.em.options" => array(
+        "mappings" => array(
+            // Using actual filesystem paths
+            array(
+                "type" => "annotation",
+                "namespace" => "Sample",
+                "path" => __DIR__."/entities",
+            ),
+            /**
+             * Add other mapping arrays here, just as above, for each namespace
+             */
+          
+            // Using PSR-0 namespaceish embedded resources
+            // (requires registering a PSR-0 Resource Locator
+            // Service Provider)
+            /*
+            array(
+                "type" => "annotation",
+                "namespace" => "Baz\Entities",
+                "resources_namespace" => "Baz\Entities",
+            ),
+            array(
+                "type" => "xml",
+                "namespace" => "Bar\Entities",
+                "resources_namespace" => "Bar\Resources\mappings",
+            ),
+            */
+        ),
+    )
+));
 
 // Register Twig
 $app->register(new \Silex\Provider\TwigServiceProvider(), array(
@@ -52,16 +88,26 @@ $app['controllers']
  */ 
 
 /**
- * Route for Page 2 (a sample page)
+ * Route for User (a sample page using the ORM)
  */
-$app->get('/page2', function() use($app) { 
+$app->get('/user', function() use($app) { 
     /**
-     * Execute a query, if needed
+     * Execute a query using the DBAL, if needed
      */
-    //$sql = 'select * from blah where field1 = ?';
-    //$data = $app['db']->executeQuery($sql, array('somevalue'));
-    //$response = $app['twig']->render('home.twig.html'); //use this line and comment the next one when we no longer need the "coming soon" page
-    $response = $app['twig']->render('page2.twig.html'); //$data would be in the second param array, if needed
+    /*
+    $sql = 'select * from users';
+    $stmt = $app['db']->executeQuery($sql);
+    $data = $stmt->fetchAll();
+    //echo '<pre>' . print_r($data, true) . '</pre>';
+    */
+
+    /**
+     * Or use the ORM
+     */
+    $dql = 'select u from Sample\User u';
+    $stmt = $app['orm.em']->createQuery($dql);
+    $users = $stmt->getResult();
+    $response = $app['twig']->render('user.index.twig.html', array('users' => $users));
     return $response;
 });
 
@@ -73,12 +119,6 @@ $app->get('/page2', function() use($app) {
  * Route for Home page
  */
 $app->get('/', function() use($app) { 
-    /**
-     * Execute a query, if needed
-     */
-    //$sql = 'select * from blah where field1 = ?';
-    //$data = $app['db']->executeQuery($sql, array('somevalue'));
-    //$response = $app['twig']->render('home.twig.html'); //use this line and comment the next one when we no longer need the "coming soon" page
     $response = $app['twig']->render('home.twig.html'); //$data would be in the second param array, if needed
     return $response;
 });
